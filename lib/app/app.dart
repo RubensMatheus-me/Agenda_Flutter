f292da986f8b1386e022a1a4e9e896212a608b03
Rubens Matheus <rubs.maths@gmail.com>
2024-06-11 00:00:58 -0300

    Aulas 10-14

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_agenda/app/view/home_page.dart';
+import 'package:flutter_agenda/app/view/contact_form.dart';
+import 'package:flutter_agenda/app/view/contact_list.dart';
 
 class MyApp extends StatelessWidget {
+  static const HOME = '/';
+  static const CONTACT_FORM = 'contact-form';
   const MyApp({super.key});
 
   // This widget is the root of your application.
@@ -10,25 +13,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {
+        HOME: (context) => ContactList(),
+        CONTACT_FORM: (context) => ContactForm()
+      },
     );
   }
 }
---
+++ lib/app/view/contact_list.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_agenda/app/app.dart';
+
+class ContactList extends StatelessWidget{
+  final list = [
+    {'nome':'Pietro','telefone':'(11) 9 9874-5656','avatar':'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png'},
+    {'nome':'Rubens','telefone':'(44) 9 9712-2777','avatar':'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png'},
+    {'nome':'Joana','telefone':'(11) 9 9562-3356','avatar':'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png'},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de Contatos'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed:() {
+              Navigator.of(context).pushNamed(MyApp.CONTACT_FORM);
+            }
+            )
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: list.length,
+        itemBuilder:(context, i){
+          var contact = list[i];
+          var avatar = CircleAvatar( backgroundImage: NetworkImage(contact['avatar'] ?? 'Dont have avatar'),);
+          return ListTile(
+            leading: avatar,
+            title: Text(contact['nome']  ?? 'Unknown Contact'),
+            subtitle: Text(contact['telefone']?? 'No phone number'),
+            trailing: Container(
+              width: 100,
+              child: const Row(
+                children: [
+                  IconButton(icon: Icon(Icons.edit), onPressed: null,),
+                  IconButton(icon: Icon(Icons.delete), onPressed: null,),
+                ],
+                ),
+            ),
+          );
+        },
+      )
+    );
+  }
+}
