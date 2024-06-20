d351ef163a4b76fb0e15eab3113018ab066e3387
Rubens Matheus <y2afepolfxx@gmail.com>
2024-06-20 12:49:43 -0300

    rebuild com Mobx

--- lib/app/injection.dart
+++ lib/app/injection.dart
@@ -1,9 +1,11 @@
 import 'package:flutter_agenda/app/database/sqlite/dao/contact_dao_impl.dart';
 import 'package:flutter_agenda/app/domain/interface/contact_dao.dart';
+import 'package:flutter_agenda/app/domain/services/contact_service.dart';
 import 'package:get_it/get_it.dart';
 
 setupInjection() {
   GetIt getIt = GetIt.I;
 
   getIt.registerSingleton<ContactDAO>(ContactDAOImpl());
+  getIt.registerSingleton<ContactService>(ContactService());
 }
---
+++ lib/app/view/contact_list_back.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_agenda/app/app.dart';
+import 'package:flutter_agenda/app/domain/entities/contact.dart';
+import 'package:flutter_agenda/app/domain/services/contact_service.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mobx/mobx.dart';
+
+
+part 'contact_list_back.g.dart';
+
+class ContactListBack = _ContactListBack with _$ContactListBack;
+
+abstract class _ContactListBack with Store{
+  var _service = GetIt.I.get<ContactService>();
+
+
+//definir lista de contatos
+  @observable
+  Future<List<Contact>> ?list;
+  
+//ter um metodo para atualizar a lista de contatos
+@action
+refreshList([dynamic value]) {
+  list = _service.find();
+}
+_ContactListBack(){
+  refreshList();
+}
+//ter um metodo para chamar o form salvar/alterar
+goToForm(BuildContext context, [Contact ?contact]) {
+  Navigator.of(context).pushNamed(MyApp.CONTACT_FORM, arguments: contact).then(refreshList);
+}
+//excluir
+remove(int id) {
+  _service.remove(id);
+  refreshList();
+}
+}
